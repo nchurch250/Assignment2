451ef6189532ea6b29d0ec0eee506b01a5cbba1f
nchurch250 <158090292+nchurch250@users.noreply.github.com>
2024-04-05 20:35:26 -0500

    fh

--- cart_application/src/index.js
+++ cart_application/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ProductPage from './script';
+import Store from './script';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-    <ProductPage />
+    <Store />
   </React.StrictMode>
 );
 
--- cart_application/src/script.js
+++ cart_application/src/script.js
@@ -1,54 +1,76 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import items from "./products.json"
 
-
-
-function ProductPage() {
+function Store() {
     const [cart, setCart] = useState([]);
     const [cartTotal, setCartTotal] = useState(0);
+    const [oneView, setOneView] = useState(false);
 
-    const addToCart = (el) => {
-        setCart([...cart, el]);
-    };
 
-    const removeFromCart = (el) => {
-        let itemFound = false;
-        const updatedCart = cart.filter((cartItem) => {
-            if (cartItem.id === el.id && !itemFound) {
-                itemFound = true;
-                return false;
+    function ProductPage() {
+
+
+        const addToCart = (el) => {
+            setCart([...cart, el]);
+        };
+
+        const removeFromCart = (el) => {
+            let itemFound = false;
+            const updatedCart = cart.filter((cartItem) => {
+                if (cartItem.id === el.id && !itemFound) {
+                    itemFound = true;
+                    return false;
+                }
+                return true;
+            });
+            if (itemFound) {
+                setCart(updatedCart);
             }
-            return true;
-        });
-        if (itemFound) {
-            setCart(updatedCart);
-        }
+        };
+
+        const listItems = items.map((el) => (
+            <div key={el.id}>
+                <img class="img-fluid" src={el.image} width={150} /> <br />
+                {el.title} <br />
+                {el.category} <br />
+                {el.price} <br />
+
+                <button type="button" onClick={() => removeFromCart(el)}>-</button>{" "}
+                <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+
+            </div>
+        ));
+
+        return (<div>
+            {listItems}
+        </div>);
+    }
+
+    function CartPage() {
+
+    }
+
+    function OrderPage() {
+
+    }
+
+    const setView = () =>{
+
+    }
+
+    const setView1 = () => {
+        if (oneView === 0) setOneView(1)
+        else if (oneView === 1) setOneView(2)
+        else setOneView(0);
     };
 
-    const listItems = items.map((el) => (
-        <div key={el.id}>
-            <img class="img-fluid" src={el.image} width={150} /> <br />
-            {el.title} <br />
-            {el.category} <br />
-            {el.price} <br />
+    return(<div>
+        <button onClick={setView1}>One</button>
 
-            <button type="button" onClick={() => removeFromCart(el)}>-</button>{" "}
-            <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
-
-        </div>
-    ));
-
-    return (<div>
-        {listItems}
+        {oneView === 0 && <ProductPage />}
+        {oneView === 1 && <CartPage />}
+        {oneView === 2 && <OrderPage />}
     </div>);
 }
 
-function CartPage() {
-
-}
-
-function OrderPage() {
-
-}
-
-export default ProductPage;
+export default Store;
