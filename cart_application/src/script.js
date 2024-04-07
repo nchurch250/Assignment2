99f0ab7341511d465a9e210167eaf523e58354f2
Dalton Clark <dbclark@iastate.edu>
2024-04-07 16:12:16 -0500

    fuck this assignment

--- cart_application/src/script.js
+++ cart_application/src/script.js
@@ -18,6 +18,56 @@ function Store() {
             total();
         }, [cart]);
 
+        const render_products = (ProductsCategory) => {
+            return (
+              <div className="category-section fixed">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">
+                  Products ({ProductsCategory.length})
+                </h2>
+                <div
+                  className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10"
+                  style={{ maxHeight: "800px", overflowY: "scroll" }}
+                >
+                  {/* Loop Products */}
+                  {ProductsCategory.map((product, index) => (
+                    <div key={index} className="group relative shadow-lg">
+                      <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
+                        <img
+                          alt="Product Image"
+                          src={product.image}
+                          className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                        />
+                      </div>
+                      <div className="flex justify-between p-3">
+                        <div>
+                          <h3 className="text-sm text-gray-700">
+                            <a href={product.href}>
+                              <span aria-hidden="true" className="absolute inset-0" />
+                              <span style={{ fontSize: "16px", fontWeight: "600" }}>
+                                {product.title}
+                              </span>
+                            </a>
+                            <div class="col">
+                                <button type="button" variant="light" onClick={() => removeFromCart(product)} > - </button>{" "}
+                                <button type="button" variant="light" onClick={() => addToCart(product)}> + </button>
+                            </div>
+                            <p>Tag - {product.category}</p>
+                          </h3>
+                          <p className="mt-1 text-sm text-gray-500">
+                            Rating: {product.rating.rate}
+                          </p>
+                        </div>
+                        <p className="text-sm font-medium text-green-600">
+                          ${product.price}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            );
+          };
+
         const total = () => {
             let totalVal = 0;
             for (let i = 0; i < cart.length; i++) {
@@ -72,9 +122,19 @@ function Store() {
             return hmot.length;
         }
 
+        const handleChange = (e) => {
+            setQuery(e.target.value);
+            console.log("Step 6 : in handleChange, Target Value :",e.target.value," Query Value :",query);
+            const results = items.filter(eachProduct => {
+            if (e.target.value === "") return ProductsCategory;
+            return eachProduct.title.toLowerCase().includes(e.target.value.toLowerCase())
+            });
+            setProductsCategory(results);
+            }
+
         return (
             <div>
-                INSERT TEXT HERE
+                <input type="search" value={query} onChange={handleChange} />
                 <div class="card">
                     <div class="row">
                         {/* HERE, IT IS THE SHOPING CART */}
@@ -91,7 +151,7 @@ function Store() {
                                     </div>
                                 </div>
                             </div>
-                            <div>{listItems}</div>
+                            <div>{render_products(ProductsCategory)}</div>
                         </div>
                         <div class="float-end">
                             <p class="mb-0 me-5 d-flex align-items-center">
@@ -202,6 +262,7 @@ function Store() {
         {oneView === 0 && <button onClick={setView}>Cart</button>}
         {oneView === 1 && <button onClick={returnView}>Back to Products</button>}
         {oneView === 2 && <button onClick={returnView}>Return</button>}
+
 
         {oneView === 0 && <ProductPage />}
         {oneView === 1 && <CartPage />}
